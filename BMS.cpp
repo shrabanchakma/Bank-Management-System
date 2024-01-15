56292d132147694490ab2055094a451a57f69969
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-15 22:45:18 +0600

    switch cases removed

--- BMS.cpp
+++ BMS.cpp
@@ -44,71 +44,71 @@ void Bank_Account::display_account()
 
 int main()
 {
-    char ch;
-    int num;
+    // char ch;
+    // int num;
 
-    cout << "\t\t --------------------------------------------------" << endl;
-    cout << "\t\t |       Welcome To Bank Management System        |" << endl;
-    cout << "\t\t --------------------------------------------------" << endl;
+    // cout << "\t\t --------------------------------------------------" << endl;
+    // cout << "\t\t |       Welcome To Bank Management System        |" << endl;
+    // cout << "\t\t --------------------------------------------------" << endl;
 
-    cout << endl;
-    cout << "\t---- Main Menu----" << endl;
-    cout << "\t 1. Create Account " << endl;
-    cout << "\t 2. Deposit Money " << endl;
-    cout << "\t 3. Withdraw Money " << endl;
-    cout << "\t 4. Balance Enquiry " << endl;
-    cout << "\t 5. All Account Holder List " << endl;
-    cout << "\t 6. Close an Account " << endl;
-    cout << "\t 7. Modify an Account " << endl;
-    cout << "\t 8. Exit " << endl;
-    cout << endl;
-    cout << "\t Enter Your Choice: (1-8)  ";
-    cin >> ch;
+    // cout << endl;
+    // cout << "\t---- Main Menu----" << endl;
+    // cout << "\t 1. Create Account " << endl;
+    // cout << "\t 2. Deposit Money " << endl;
+    // cout << "\t 3. Withdraw Money " << endl;
+    // cout << "\t 4. Balance Enquiry " << endl;
+    // cout << "\t 5. All Account Holder List " << endl;
+    // cout << "\t 6. Close an Account " << endl;
+    // cout << "\t 7. Modify an Account " << endl;
+    // cout << "\t 8. Exit " << endl;
+    // cout << endl;
+    // cout << "\t Enter Your Choice: (1-8)  ";
+    // cin >> ch;
 
-    switch (ch)
-    {
-    case '1':
-        break;
-    case '2':
-        system("cls");
-        cout << "\t Enter the account Number: ";
-        cin >> num;
-        // deposit function;
-        break;
-    case '3':
-        system("cls");
-        cout << "\t Enter the account Number: ";
-        cin >> num;
-        // withdraw function;
-        break;
-    case '4':
-        system("cls");
-        cout << "\t Enter the account Number: ";
-        cin >> num;
-        // enquiry function;
-        break;
-    case '5':
-        // display_all() function to display all users list
-        break;
-    case '6':
-        system("cls");
-        cout << "\t Enter the account Number: ";
-        cin >> num;
-        // delete account function;
-        break;
-    case '7':
-        system("cls");
-        cout << "\t Enter the account Number: ";
-        cin >> num;
-        // modify account function;
-        break;
-    case '8':
-        cout << "\t Thanks for using the Bank Management System" << endl;
-        break;
-    }
-    Bank_Account B;
-    B.create_account();
-    B.display_account();
+    // switch (ch)
+    // {
+    // case '1':
+    //     break;
+    // case '2':
+    //     system("cls");
+    //     cout << "\t Enter the account Number: ";
+    //     cin >> num;
+    //     // deposit function;
+    //     break;
+    // case '3':
+    //     system("cls");
+    //     cout << "\t Enter the account Number: ";
+    //     cin >> num;
+    //     // withdraw function;
+    //     break;
+    // case '4':
+    //     system("cls");
+    //     cout << "\t Enter the account Number: ";
+    //     cin >> num;
+    //     // enquiry function;
+    //     break;
+    // case '5':
+    //     // display_all() function to display all users list
+    //     break;
+    // case '6':
+    //     system("cls");
+    //     cout << "\t Enter the account Number: ";
+    //     cin >> num;
+    //     // delete account function;
+    //     break;
+    // case '7':
+    //     system("cls");
+    //     cout << "\t Enter the account Number: ";
+    //     cin >> num;
+    //     // modify account function;
+    //     break;
+    // case '8':
+    //     cout << "\t Thanks for using the Bank Management System" << endl;
+    //     break;
+    // }
+    // Bank_Account B;
+    // B.create_account();
+    // B.display_account();
 
     return 0;
 }
