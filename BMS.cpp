a2e09e59ed4f87fc52d59d9f51c21ae20c3e038f
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-15 23:56:43 +0600

    create_account and display_account function removed

--- BMS.cpp
+++ BMS.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <cctype>
+#include <conio.h>
 
 using namespace std;
 
@@ -9,11 +7,11 @@ class Bank_Account
 {
 private:
     int total;
-    struct person
+    struct user
     {
         string name, ID, address;
         int contact, cash;
-    } person[100];
+    } user[100];
 
 public:
     Bank_Account()
@@ -21,32 +19,9 @@ public:
         total = 0;
     }
     void choice();
+    void userData();
+    void show();
 };
-
-void Bank_Account::create_account()
-{
-    system("cls");
-    cout << "\t Enter the account number: ";
-    cin >> account_number;
-    cout << "\t Enter the name of the account holder: ";
-    cin.ignore();
-    cin.getline(name, 50);
-    cout << "\t Enter the type of the account (C/S): ";
-    cin >> type;
-    type = toupper(type);
-    cout << "\t Enter the initial amount (>= 500 for saving and >=1000 for current): ";
-    cin >> Money_Deposit;
-    cout << "\t Account Created....... :)";
-    cout << endl
-         << endl;
-}
-void Bank_Account::display_account()
-{
-    cout << "\t Bank Account Number: " << account_number << endl;
-    cout << "\t Account Holder Name: " << name << endl;
-    cout << "\t Type of Account: " << type << endl;
-    cout << "\t Balance Amount: " << Money_Deposit << endl;
-}
 
 int main()
 {
@@ -65,13 +40,48 @@ int main()
 void Bank_Account::choice()
 {
     char ch;
-    cout << "\t---- Main Menu----" << endl;
-    cout << "\t 1. Create Account " << endl;
-    cout << "\t 5. All Account Holder List " << endl;
-    cout << "\t 7. Modify an Account " << endl;
-    cout << "\t 4. Balance Enquiry " << endl;
-    cout << "\t 2. Deposit Money " << endl;
-    cout << "\t 3. Withdraw Money " << endl;
-    cout << "\t 6. Close Existing Account " << endl;
-    cout << "\t 8. Exit " << endl;
+    while (1)
+    {
+        cout << "\t---- Main Menu----" << endl;
+        cout << "\t 1. Create Account " << endl;
+        cout << "\t 2. All Account Holder List " << endl;
+        cout << "\t 3. Modify an Account " << endl;
+        cout << "\t 4. Balance Enquiry " << endl;
+        cout << "\t 5. Deposit Money " << endl;
+        cout << "\t 6. Withdraw Money " << endl;
+        cout << "\t 7. Close Existing Account " << endl;
+        cout << "\t 8. Exit " << endl;
+        ch = getchar();
+        switch (ch)
+        {
+        case 1:
+            Bank_Account::userData();
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+void Bank_Account::userData()
+{
+    cout << "\t\t Enter User Information : \n\n";
+    cout << "\t Name: ";
+    cin >> user[total].name;
+    cout << "\t ID: ";
+    cin >> user[total].ID;
+    cout << "\t Address: ";
+    cin >> user[total].address;
+    cout << "\t Contact Number: ";
+    cin >> user[total].contact;
+    cout << "\t Deposit cash amount: ";
+    cin >> user[total].cash;
+    total++;
+    cout << endl
+         << endl;
+}
+
+void Bank::show()
+{
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,6 @@
-
-    // B.create_account();
-    // B.display_account();
+{
+//     cout << "\t Bank Account Number: " << account_number << endl;
+//     cout << "\t Account Holder Name: " << name << endl;
+//     cout << "\t Type of Account: " << type << endl;
+//     cout << "\t Balance Amount: " << Money_Deposit << endl;
+// }
