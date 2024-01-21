11ca36b86a76f96ef28b27d9a8210e921dca0b71
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-21 20:26:56 +0600

    updated createUser to dinamically store users

--- BMS.cpp
+++ BMS.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include <conio.h>
-
+#include <windows.h>
 using namespace std;
 
 class Bank_Account
 {
 private:
     int total;
-    string id;
+    string name, ID, address;
+    int contactNumber, cash;
     struct user
     {
         string name, ID, address;
-        int contact, cash;
-    } user[100];
+        int contactNumber, cash;
+        user *prev;
+        user *next;
+    };
+
+    user *Head = NULL;
 
 public:
     Bank_Account()
@@ -20,22 +25,24 @@ public:
         total = 0;
     }
     void choice();
-    void userData();
-    void show();
-    void update();
+    void createUser();
+    void setUser(user *newUser);
+    void showUser();
+    //   void update();
+    //     void search();
+    //     void transaction();
 };
 
 int main()
 {
-    // char ch;
-    // int num;
 
-    // cout << "\t\t --------------------------------------------------" << endl;
-    // cout << "\t\t |       Welcome To Bank Management System        |" << endl;
-    // cout << "\t\t --------------------------------------------------" << endl;
+    cout << "\t\t --------------------------------------------------" << endl;
+    cout << "\t\t |       Welcome To Bank Management System        |" << endl;
+    cout << "\t\t --------------------------------------------------" << endl
+         << endl;
 
-    // cout << endl;
-
+    Bank_Account executeProgram;
+    executeProgram.choice();
     return 0;
 }
 
@@ -46,86 +53,103 @@ void Bank_Account::choice()
     {
         cout << "\t---- Main Menu----" << endl;
         cout << "\t 1. Create Account " << endl;
-        cout << "\t 2. All Account Holder List " << endl;
+        cout << "\t 2. Show All Users" << endl;
         cout << "\t 3. Modify an Account " << endl;
         cout << "\t 4. Balance Enquiry " << endl;
         cout << "\t 5. Deposit Money " << endl;
         cout << "\t 6. Withdraw Money " << endl;
         cout << "\t 7. Close Existing Account " << endl;
         cout << "\t 8. Exit " << endl;
-        ch = getchar();
+        cin >> ch;
         switch (ch)
         {
         case '1':
-            Bank_Account::userData();
+            Bank_Account::createUser();
             break;
         case '2':
-            Bank_Account::show();
+            Bank_Account::showUser();
             break;
-
+        // case '3':
+        //     Bank_Account::update();
+        //     break;
+        // case '4':
+        //     Bank_Account::search();
+        //     break;
         default:
             break;
         }
     }
 }
 
-void Bank_Account::userData()
+void Bank_Account::createUser()
 {
-    cout << "\t\t Enter User Information : \n\n";
-    cout << "\t Name: ";
-    cin >> user[total].name;
-    cout << "\t ID: ";
-    cin >> user[total].ID;
-    cout << "\t Address: ";
-    cin >> user[total].address;
-    cout << "\t Contact Number: ";
-    cin >> user[total].contact;
-    cout << "\t Deposit cash amount: ";
-    cin >> user[total].cash;
-    total++;
-    cout << endl
-         << endl;
-}
-
-void Bank_Account::show()
-{
-    for (int i = 0; i < total; i++)
+    if (Head == NULL)
     {
-        cout << "Data of User " << i + 1 << endl;
-        cout << "Name: " << user[i].name << endl;
-        cout << "ID: " << user[i].ID << endl;
-        cout << "Address: " << user[i].address << endl;
-        cout << "Balance: " << user[i].cash << endl;
+        user *newUser = new user();
+        cout << "\t\t Enter User Information : \n\n";
+        cout << "\t Name: ";
+        cin >> name;
+        newUser->name = name;
+        cout << "\t ID: ";
+        cin >> ID;
+        newUser->ID = ID;
+        cout << "\t Address: ";
+        cin >> address;
+        newUser->address;
+        cout << "\t Contact Number: ";
+        cin >> contactNumber;
+        newUser->contactNumber = contactNumber;
+        cout << "\t Deposit cash amount: ";
+        cin >> cash;
+        newUser->cash = cash;
+        newUser->prev = NULL;
+        newUser->next = NULL;
+        Head = newUser;
+        cout << endl
+             << endl;
+    }
+    else
+    {
+        user *newUser = new user();
+        cout << "\t\t Enter User Information : \n\n";
+        cout << "\t Name: ";
+        cin >> name;
+        newUser->name = name;
+        cout << "\t ID: ";
+        cin >> ID;
+        newUser->ID = ID;
+        cout << "\t Address: ";
+        cin >> address;
+        newUser->address;
+        cout << "\t Contact Number: ";
+        cin >> contactNumber;
+        newUser->contactNumber = contactNumber;
+        cout << "\t Deposit cash amount: ";
+        cin >> cash;
+        newUser->cash = cash;
+        newUser->prev = NULL;
+        newUser->next = NULL;
+
+        setUser(newUser);
     }
 }
 
-void Bank_Account::update()
+void Bank_Account::setUser(user *newUser)
 {
-    cout << "\t---- Update Info----" << endl;
-    cout << "Enter User Id: " << endl;
-    cin >> id;
-    for (int i = 0; i < total; i++)
+    user *temp = Head;
+    while (temp->next != NULL)
     {
-        if (id == user[i].ID)
-        {
-            cout << "Previous Data" << endl;
-            cout << "Data of User " << i + 1 << endl;
-            cout << "Name: " << user[i].name << endl;
-            cout << "ID: " << user[i].ID << endl;
-            cout << "Address: " << user[i].address << endl;
-            cout << "Balance: " << user[i].cash << endl
-                 << endl;
-            cout << "Enter New Data: " << endl;
-            cout << "\t Name: ";
-            cin >> user[total].name;
-            cout << "\t ID: ";
-            cin >> user[total].ID;
-            cout << "\t Address: ";
-            cin >> user[total].address;
-            cout << "\t Contact Number: ";
-            cin >> user[total].contact;
-            cout << "\t Deposit cash amount: ";
-            cin >> user[total].cash;
-        }
+        temp = temp->next;
+    }
+    temp->next = newUser;
+}
+
+void Bank_Account::showUser()
+{
+    user *temp = Head;
+    while (temp != NULL)
+    {
+        cout << "Name of the User: " << temp->name << endl;
+        temp = temp->next;
     }
 }
